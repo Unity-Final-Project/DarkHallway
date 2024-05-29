a74586bddcbb4cb133ad3547d35e848df544ef13
CrimSonRice <92437794+CrimSonRice@users.noreply.github.com>
2024-05-29 18:46:47 +0700

    MenuDoorAnimation

--- Assets/Scripts/DoorTrigger.cs
+++ Assets/Scripts/DoorTrigger.cs
@@ -1,21 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DoorTrigger : MonoBehaviour
 {
     [SerializeField] private Animator myDoor = null;
     [SerializeField] private bool openTrigger = false;
 
-    private void OnTriggerEnter(Collider other)
+    // This method will be called when the button is clicked
+    public void TriggerDoorOpen()
     {
-        if (other.CompareTag("CameraTrigger")) // Check for the CameraTrigger tag
+        if (openTrigger)
         {
-            if (openTrigger)
-            {
-                myDoor.Play("DoorOpen", 0, 0.0f);
-                gameObject.SetActive(false);
-            }
+            myDoor.Play("DoorOpen", 0, 0.0f);
+            gameObject.SetActive(false);
         }
     }
 }
